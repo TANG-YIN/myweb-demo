702095f9f4307004d2639a2fb7f0410c03eb6a6b
root <root@PF1VAL57.localdomain>
2026-09-11 15:36:03 +0800

    look ar book!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import pymysql
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
@@ -10,25 +10,29 @@ MYSQL_USER = "root"
 MYSQL_PASSWORD = "123456"
 MYSQL_DB = "HPE_APP"
 
-HTML_TEMPLATE = """
-<!DOCTYPE html>
-<html>
-<head><meta charset="UTF-8"><title>Python Kubernetes Demo</title><head>
-<body align="center">
-    <br><br><br><br><br><br>
-    <h3>Please input your info</h3>
-    <form action="/insert" method="post">
-        Your Name: <input type="text" name="user_name"><br><br>
-        Your Level: <input type="text" name="level" value="100"><br><br>
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
-"""
-
 @app.route("/")
 def index():
-    return render_template_string(HTML_TEMPLATE)
+    try:
+        conn = pymysql.connect(
+            host=MYSQL_HOST,
+            port=MYSQL_PORT,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            db=MYSQL_DB,
+            charset='utf8mb4'
+        )
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM T_USERS"
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+        conn.close()
+    except pymysql.Error as e:
+        return f"<h3>Error: {e}</h3><br><a href='/'>Return</a>"
+    return render_template("index.html", users=rows)
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
 
 @app.route("/insert", methods=["POST"])
 def insert():
@@ -48,9 +52,9 @@ def insert():
             cursor.execute(sql, (user_name, level))
             conn.commit()
         conn.close()
-        return "<h3>Success add your info</h3><br><a href='/'>Return</a>"
+        return redirect("/")
     except pymysql.Error as e:
-        return f"<h3>Error: {e}</h3><br><a href='/'>Return</a>"
+        return f"<h3>Error: {e}</h3><br><a href='/add'>Return</a>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
