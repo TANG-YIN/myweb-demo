4f03b88e2d2a8cfe6303af703f01fbe11822f009
root <root@PF1VAL57.localdomain>
2026-09-14 17:18:24 +0800

    修复重构后的问题-路由错误-配置错误

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     MYSQL_HOST = os.getenv("MYSQL_SERVICE_HOST", "localhost")
     MYSQL_PORT = int(os.getenv("MYSQL_SERVICE_PORT", 3306))
-    MYSQL_USER = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "123456")
-    MYSQL_DB = os.getenv("MYSQL_DB", "HPE_APP")
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "123456"
+    MYSQL_DB = "HPE_APP"
